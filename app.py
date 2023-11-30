7326fe1ded1be201c25d96fecb2828442eb018e9
mahlvm <mahmoreti@hotmail.com>
2023-11-30 12:32:52 +0000

    process

--- app.py
+++ app.py
@@ -3,26 +3,50 @@ from lib.artist_repository import ArtistRepository
 from lib.album_repository import AlbumRepository
 
 
-# Connect to the database
-connection = DatabaseConnection()
-connection.connect()
+# # Connect to the database
+# connection = DatabaseConnection()
+# connection.connect()
 
-# Seed with some seed data
-connection.seed("seeds/music_library.sql")
+# # Seed with some seed data
+# connection.seed("seeds/music_library.sql")
 
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
+# # Retrieve all artists
+# artist_repository = ArtistRepository(connection)
+# artists = artist_repository.all()
 
-# List them out
-for artist in artists:
-    print(artist)
+# # List them out
+# for artist in artists:
+#     print(artist)
 
-#Print ALL rows
-album_repository = AlbumRepository(connection)
-albums = album_repository.all()
-for album in albums:
-    print(album)
+# #Print ALL rows
+# album_repository = AlbumRepository(connection)
+# albums = album_repository.all()
+# for album in albums:
+#     print(album)
 
-#Print just the choos row
-print (album_repository.find(1))
+# #Print just the choos row
+# print (album_repository.find(1))
+
+from lib.artist_repository import ArtistRepository
+from lib.database_connection import DatabaseConnection
+
+class Application():
+    def __init__(self):
+        self._connection = DatabaseConnection()
+        self._connection.connect()
+        self._connection.seed("seeds/music_library.sql")
+
+    def run(self):
+        input = input("Welcome to the music library manager! /n What would you like to do? /n 1 - List all albums /n 2 - List all artists")
+        print(input)
+    
+
+#     artist_repository = ArtistRepository(self._connection)
+#     artists = artist_repository.all()
+
+#     for artist in artists:
+#         print(f"{artist.id}: {artist.name} ({artist.genre})")
+
+if __name__ == '__main__':
+    app = Application()
+    app.run()
